279e6e6b4a9a875b0ea6cce8922793119f106a31
Conor Lawless <cnr.lwlss@gmail.com>
2019-02-28 19:27:03 +0000

    Looking at categories of fibres to try to learn about how they become defective

---
+++ shiny/deficiencyOrder.R
@@ -0,0 +1,70 @@
+install.packages("klaR")
+library(klaR)
+library(plyr)
+# Set working directory to byPatient
+source("app.R")
+
+countFibres = function(dat, pat, cord, type="theta (VDAC1)", difftype="regression_diff", position = "BELOW"){
+  dt = data.frame(updateDat(dat, type , pat, "NDUFB8", cord))
+  Nfibres = length(unique(dt$cell_id[dt$patrep_id==pat]))
+  formul = paste(difftype,"ch",sep="~")
+  res = data.frame(aggregate(eval(parse(text=formul)),data=dt,function(x) sum(x==position)),stringsAsFactors=FALSE)
+  colnames(res) = c("ch","count")
+  res = rbind(res, data.frame(ch="TOTAL",count=Nfibres))
+  return(res)
+}
+
+getcount = function(x) as.numeric(countFibres(dat,x,cord,type="theta (VDAC1)", difftype="outlier_diff", position = "BELOW")$count)
+
+cats = function(pat,dat,type,difftype,chans){
+ dt = data.frame(updateDat(dat, type , pat, "NDUFB8", chans))
+ Nfibres = length(unique(dt$cell_id[dt$patrep_id==pat]))
+ wtab = reshape(dt[,c(difftype,"ch","cell_id")],idvar="cell_id",timevar="ch",direction="wide")
+ colnames(wtab)=gsub(paste(difftype,".",sep=""),"",colnames(wtab))
+ wtab = wtab[,c("cell_id",chans)]
+ return(wtab)
+}
+
+countCategories = function(pat,dat,type,difftype,chans){
+ wtab = cats(pat,dat,type,difftype,chans)
+ combs = data.frame(unique(wtab[,2:length(colnames(wtab))]))
+ rownames(combs) = 1:length(combs[,1])
+ corder = do.call(order, as.list(wtab))
+
+ wdat = wtab[,2:length(colnames(wtab))]
+ colnames(wdat)= substr(colnames(wdat),1,4)
+ wcount = count(wdat, vars=names(wdat))
+ wcount$percent = signif(100*wcount$freq/length(wtab$cell_id),2)
+ return(wcount[order(wcount$freq,decreasing=TRUE),])
+}
+
+chs = unique(dat$ch)
+pats = unique(dat$patrep_id)
+
+ctab = sapply(pats,getcount)
+rownames(ctab) = c(cord,"TOTAL")
+ctab = data.frame(t(ctab))
+ctab$VDAC1=NULL
+
+ptab = signif(100*sweep(ctab,1,ctab$TOTAL,"/"),2)
+ptab$TOTAL=NULL
+
+neworder = sort(colSums(ptab),decreasing=TRUE)
+ptab = ptab[,names(neworder)]
+ctab = ctab[,c(names(neworder),"TOTAL")]
+
+chans = names(neworder)
+chans = c("NDUFB8", "MTCO1", "UqCRC2")
+chans = gsub("\\.","+",chans)
+
+for(pat in unique(dat$patrep_id)){
+ print(pat)
+ print(countCategories(pat,dat,type="theta (VDAC1)", difftype="regression_diff",chans))
+}
+
+wtab = cats("P03R01",dat,type,difftype,chans)
+wtab = wtab[,2:length(colnames(wtab))]
+kmodes(wtab, 5, iter.max = 1000, weighted = FALSE )
+
+
+

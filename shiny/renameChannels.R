7aa8b4ebae9375f0c79454d29ee4886536511f72
Conor Lawless <cnr.lwlss@gmail.com>
2024-02-09 19:05:12 +0000

    Had accidentally renamed alternatives log_ med_ etc. as the raw channel value

--- shiny/renameChannels.R
+++ shiny/renameChannels.R
@@ -1,4 +1,10 @@
 dat=read.delim("Warren_2020.csv",sep=",",stringsAsFactors=FALSE)
-getprot = function(ch) ifelse(grepl(" ",ch),strsplit(ch," ")[[1]][2],ch)
-dat$Channel=sapply(dat$Channel,getprot)
+metals = c("153Eu_153Eu ","158Gd_158Gd ","160Gd_160Gd ","161Dy_161Dy ","164Dy_164Dy ","166Er_166Er ","168Er_168Er ","172Yb_172Yb ","174Yb_174Yb ","176Yb_176Yb ")
+
+getprot = function(ch,metals) {
+  for(met in metals) ch = gsub(met,"",ch)
+  return(ch)
+}
+
+dat$Channel=sapply(dat$Channel,getprot,metals)
 write.table(dat,"Warren_2020_chans.csv",sep=",",quote=FALSE,row.names=FALSE)
